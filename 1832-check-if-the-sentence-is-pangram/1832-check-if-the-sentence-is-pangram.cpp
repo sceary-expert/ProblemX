c1f7fd344ca2734c5f84660ad6ff50c5ec69a2ca
Dhananjoy Dey <84634405+sceary-expert@users.noreply.github.com>
2022-10-17 15:40:06 +0530

    Time: 3 ms (80.43%), Space: 6.3 MB (98.63%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -1,12 +1,1 @@
-class Solution {
-public:
-    bool checkIfPangram(string sentence) {
-        vector<int>v(26, 0);
-        for(auto &c : sentence )
-        {
-            v[c - 'a']++;
-        }
-        for(auto &it : v)if(it == 0)return false;
-        return true;
-    }
-};
+class Solution {public:bool checkIfPangram(string sentence){vector<int>v(26, 0);for(auto &c : sentence ){v[c - 'a']++;}for(auto &it : v)if(it == 0)return false;return true;}};

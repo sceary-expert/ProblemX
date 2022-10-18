09bd5e24a2cceb461420290790074a7a4dfb4967
Dhananjoy Dey <84634405+sceary-expert@users.noreply.github.com>
2022-10-18 19:32:45 +0530

    Time: 16 ms (52.36%), Space: 7 MB (36.18%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -2,32 +2,24 @@ class Solution {
 public:
         string convert(string prev)
         {
-                string ans = "";
-                
+                string ans = "";                
                 int f = 0, p = 0, n = prev.size();
                 while(p < n)
                 {
                         char c = prev[p];//2
                         f = 0;
-                        //if(prev == "21")cout<<"c : "<<c<<endl;
                         while(p < n && prev[p] == c)
                         {
-                                f++;
-                                p++;
+                                f++; p++;
                         }
-                        ans += to_string(f);
-                        ans += c;
-                        //if(prev == "21")cout<<"f : "<<f<<endl;
+                        ans += to_string(f);ans += c;                       
                 }
-                return ans;               
-                
+                return ans;                               
         }
     string countAndSay(int n) {
         if(n == 1)return "1";
-            string prev = countAndSay(n - 1);
-            //cout<<"prev : "<<prev<<endl;
-            string ans = convert(prev);
-            //cout<<"ans : "<<ans<<endl;
+            string prev = countAndSay(n - 1);            
+            string ans = convert(prev);            
             return ans;
     }
 };

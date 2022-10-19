4c5941dcdfbc1790f0a8035eac672ed02ed11a8d
Dhananjoy Dey <84634405+sceary-expert@users.noreply.github.com>
2022-10-19 10:26:49 +0530

    Time: 4 ms (30.46%), Space: 6.6 MB (28.81%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     
-    int findPath(int x, int y, int m, int n, vector<vector<int>>& dp)
+    int findPath(int x, int y, vector<vector<int>>& dp)
     {
         if(x == 0 && y == 0)return 1;
         if(x < 0 || y < 0)return 0;
         if(dp[x][y] != -1)return dp[x][y];
-        int k = (findPath(x - 1, y,m, n, dp) + findPath(x, y - 1, m, n, dp));      
+        int k = (findPath(x - 1, y, dp) + findPath(x, y - 1, dp));      
             return dp[x][y] = k ;
     }
     int uniquePaths(int m, int n) {
         vector<vector<int>>dp(m, vector<int>(n, -1));
-        return findPath(m - 1, n - 1, m, n, dp);
+        return findPath(m - 1, n - 1, dp);
     }
 };

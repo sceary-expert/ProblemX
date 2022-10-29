6d12fd6cf7ed3997d64a82c998a6ab61d1596bc7
Dhananjoy Dey <84634405+sceary-expert@users.noreply.github.com>
2022-10-29 13:27:43 +0530

    Time: 586 ms (66.87%), Space: 168.5 MB (62.25%) - LeetHub

--- 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.cpp
+++ 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
     int earliestFullBloom(vector<int>& plantTime, vector<int>& growTime) {
-        int pt = 0;
-        int ans = 0;
-        int n = plantTime.size();
+        int pt = 0, ans = 0, n = plantTime.size();
         priority_queue<pair<int, int>>pq;
-        for(int i = 0; i < n; i++)
-        {
-            pq.push({growTime[i], plantTime[i]});
-        }
-        //for(auto &ptm : plantTime)
-        //for(int i = 0; i < plantTime.size(); i++)
+        for(int i = 0; i < n; i++)pq.push({growTime[i], plantTime[i]});
         while(pq.size())
-        {
-            
+        {        
             int ptm = pq.top().second;//plantTime[i];
             pt += ptm;
             int curr = pt + pq.top().first;//growTime[i];
             if(curr > ans)ans = curr;       
             pq.pop();
         }
-        return ans;
-        
+        return ans;        
     }
 };

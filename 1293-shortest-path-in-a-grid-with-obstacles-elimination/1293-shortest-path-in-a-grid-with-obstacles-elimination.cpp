c530bdf0f82f920b67829e191516d7af3eae9684
Dhananjoy Dey <84634405+sceary-expert@users.noreply.github.com>
2022-10-30 21:22:38 +0530

    Time: 549 ms (23.93%), Space: 58.6 MB (18.65%) - LeetHub

---
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    //#define pii pair<int, pair<int, int>>
+    bool isValid(int ni, int  nj, int m, int  n)
+    {
+        if(ni < 0 || nj < 0 || ni >= m || nj >= n )return false;
+        return true;
+    }
+    int shortestPath(vector<vector<int>>& grid, int k) {
+        int m = grid.size(), n = grid[0].size(), steps = 0;
+       // bool vis[m + 1][n + 1][k + 1];
+        vector<vector<vector<bool>>>vis(m + 1,  vector<vector<bool>>(n + 1, vector<bool>(k + 1, false)));
+        queue<vector<int>>q;
+        
+        //q.push({0, {0, k}});
+        q.push({0, 0, k});
+        vis[0][0][k] = true;
+        vector<vector<int>>dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+        while(q.size())
+        {
+            int sz = q.size();
+            for(int o = 0; o < sz; o++)
+            {
+                auto t = q.front();
+                q.pop();     
+                
+                int i = t[0];
+                int j = t[1];
+                int l = t[2];
+                if(i == m - 1 && j == n - 1 && l >= 0)return steps;
+                for(auto &d : dir)
+                {
+                    int ni = i + d[0];
+                    int nj = j + d[1];
+                    int nl = l;
+                    
+                    if(isValid(ni, nj, m, n))
+                    {
+                        if(grid[ni][nj] == 1)nl -= grid[ni][nj];
+                        if(nl >= 0 && vis[ni][nj][nl] == false)
+                        {
+                            vis[ni][nj][nl] = true;
+                            q.push({ni, nj, nl});
+                        }
+                        
+                    }
+                }
+                
+                
+                
+                
+            }
+            steps++;
+           
+            
+            
+        }
+        return -1;
+        
+    }
+};

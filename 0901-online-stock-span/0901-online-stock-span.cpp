b083c83bff21016ff7322d3eb4c2ca3c225780b2
Dhananjoy Dey <84634405+sceary-expert@users.noreply.github.com>
2022-11-09 19:39:12 +0530

    Time: 647 ms (5.06%), Space: 84.2 MB (90.79%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.cpp
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -1,7 +1,30 @@
-class StockSpanner {public:    stack<pair<int, int>>st;    StockSpanner() {} int next(int price) {int ans = 1;while(st.size() && st.top().first <= price)ans += st.top().second, st.pop();st.push({price, ans});return ans;}};
-
-/**
- * Your StockSpanner object will be instantiated and called as such:
- * StockSpanner* obj = new StockSpanner();
- * int param_1 = obj->next(price);
- */
+class StockSpanner {
+public:
+    stack<pair<int, int>> s;
+    int i=0;
+ 
+    int next(int price) {
+       if(s.empty()){
+           s.push({price, i++});
+           return 1;
+       }
+        else{
+            
+            // now our stack is not empty so we can eleminate smaller element
+            
+            while(!s.empty() && s.top().first <= price ){
+                s.pop();
+            }
+            int ans;
+            
+            // if our stack is empty so
+            if(s.empty()) ans = i+1; // case for 105 in [100, 80, 70, 60, 105]
+            else{
+                ans = i- s.top().second;
+            }
+            s.push({price, i++});
+            return ans;
+        }
+    
+    }
+};

ef63511e842df3edbebebb33103f68fecfa9bf82
Dhananjoy Dey <84634405+sceary-expert@users.noreply.github.com>
2022-11-09 19:38:06 +0530

    Time: 510 ms (35.73%), Space: 84.2 MB (60.87%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.cpp
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -1,6 +1,4 @@
-class StockSpanner {
-public:    stack<pair<int, int>>st;    StockSpanner() {} int next(int price) {int ans = 1;while(st.size() && st.top().first <= price)ans += st.top().second, st.pop();st.push({price, ans});return ans;}
-};
+class StockSpanner {public:    stack<pair<int, int>>st;    StockSpanner() {} int next(int price) {int ans = 1;while(st.size() && st.top().first <= price)ans += st.top().second, st.pop();st.push({price, ans});return ans;}};
 
 /**
  * Your StockSpanner object will be instantiated and called as such:

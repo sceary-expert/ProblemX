a31205910c035d6ed34240a834b5a683b351e12f
Dhananjoy Dey <84634405+sceary-expert@users.noreply.github.com>
2022-11-09 19:37:50 +0530

    Time: 525 ms (28.30%), Space: 84.2 MB (60.87%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.cpp
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -1,7 +1,5 @@
 class StockSpanner {
-public:    stack<pair<int, int>>st;    StockSpanner() {  
-    }
-    int next(int price) {int ans = 1;while(st.size() && st.top().first <= price)ans += st.top().second, st.pop();st.push({price, ans});return ans;}
+public:    stack<pair<int, int>>st;    StockSpanner() {} int next(int price) {int ans = 1;while(st.size() && st.top().first <= price)ans += st.top().second, st.pop();st.push({price, ans});return ans;}
 };
 
 /**

4d2dbe271de2a32d8a501a9957f8e7f08932fab3
Dhananjoy Dey <84634405+sceary-expert@users.noreply.github.com>
2022-11-30 13:13:58 +0530

    Time: 5 ms (38.52%), Space: 42 MB (51.91%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -1,37 +1,18 @@
 class Solution {
+    int k;
     public boolean uniqueOccurrences(int[] arr) {
-        HashSet<Integer>set = new HashSet<>();
-        HashSet<Integer>freq ;
-        HashMap<Integer, Integer>map = new HashMap<>();
-        for(int i : arr)
+        k = 1000;
+        int freq[] = new int[2 * k + 1];
+        for(int a : arr)
         {
-            // if(map.containsKey(i))
-            // {
-            //     map.put(i, 1 + map.get(i));
-            // }
-            // else{
-            //     map.put(i, 1);
-            // }
-            map.put(i, map.getOrDefault(i, 0) + 1);
-            set.add(i);
+            freq[k + a]++;
         }
-        freq = new HashSet<>(map.values());
-        // for(int i : set)
-        // {
-        //     //System.out.println("freq " + map.get(i));
-        //     freq.add(map.get(i));
-        // }
-        //System.out.println(set.size());
-        //System.out.println(freq.size());
-        
-        return set.size() == freq.size();
-        
-        
-        //System.out.println(arr.length);
-        
-        //return set.size() == arr.length;
-        
-        
-        
+        Arrays.sort(freq);
+        for(int i = 0; i < 2 * k; i++)
+        {
+            if(freq[i] != 0 && freq[i] == freq[i + 1])return false;
+            
+        }
+        return true;
     }
 }

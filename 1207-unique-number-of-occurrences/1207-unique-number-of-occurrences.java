2fccadfe4005eb41a50e17f5154834f51b6e5363
Dhananjoy Dey <84634405+sceary-expert@users.noreply.github.com>
2022-11-30 13:03:47 +0530

    Time: 6 ms (16.01%), Space: 43 MB (6.00%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -1,24 +1,26 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
         HashSet<Integer>set = new HashSet<>();
-        HashSet<Integer>freq = new HashSet<>();
+        HashSet<Integer>freq ;
         HashMap<Integer, Integer>map = new HashMap<>();
         for(int i : arr)
         {
-            if(map.containsKey(i))
-            {
-                map.put(i, 1 + map.get(i));
-            }
-            else{
-                map.put(i, 1);
-            }
+            // if(map.containsKey(i))
+            // {
+            //     map.put(i, 1 + map.get(i));
+            // }
+            // else{
+            //     map.put(i, 1);
+            // }
+            map.put(i, map.getOrDefault(i, 0) + 1);
             set.add(i);
         }
-        for(int i : set)
-        {
-            //System.out.println("freq " + map.get(i));
-            freq.add(map.get(i));
-        }
+        freq = new HashSet<>(map.values());
+        // for(int i : set)
+        // {
+        //     //System.out.println("freq " + map.get(i));
+        //     freq.add(map.get(i));
+        // }
         //System.out.println(set.size());
         //System.out.println(freq.size());
         

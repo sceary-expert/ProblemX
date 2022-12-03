6b342e1f35016c1c30d60220ca82cfa548673c2a
Dhananjoy Dey <84634405+sceary-expert@users.noreply.github.com>
2022-12-03 12:50:26 +0530

    Time: 112 ms (6.69%), Space: 7.8 MB (98.31%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        vector<int>freq(256);
-        for(auto c : s)freq[c]++;
-        sort(s.begin(), s.end(), [&](char a, char b){
-            if(freq[a] == freq[b])return a < b;
-            else return freq[a] > freq[b];
-        });
+        // vector<int>freq(256);
+        // for(auto c : s)freq[c]++;
+        // sort(s.begin(), s.end(), [&](char a, char b){
+        //     if(freq[a] == freq[b])return a < b;
+        //     else return freq[a] > freq[b];
+        // });
+        // return s;
+        vector<int> count(256);
+        for(auto c : s)
+            count[c]++;
+        sort(s.begin(), s.end(), [&](char a, char b)
+             {
+                 if(count[a] == count[b])
+                     return a < b;
+                 return count[a] > count[b];
+             });
         return s;
         
     }

61c6820db43058d3c74d26a2b26c54a619d55841
Dhananjoy Dey <84634405+sceary-expert@users.noreply.github.com>
2023-02-06 19:21:57 +0530

    Added solution - LeetHub

---
+++ BST Downward Traversal - GFG/bst-downward-traversal.java
@@ -0,0 +1,158 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        PrintWriter out=new PrintWriter(System.out);
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+    	    	int target=Integer.parseInt(br.readLine());
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+        	    Solution g = new Solution();
+        	    long ans=g.verticallyDownBST(root,target);
+        	    out.println(ans);
+                t--;
+            
+        }
+        out.close();
+    }
+  
+}
+
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    long ans;
+    int axis;
+    Node t;
+    void findTarget(Node root, int target, int point)
+    {
+        if(axis != Integer.MIN_VALUE || root == null)return;
+        
+        if(root.data == target)
+        {
+            axis = point;
+            t = root;
+        }
+        
+        if(root.data < target)findTarget(root.right, target, point + 1);
+        if(root.data > target)findTarget(root.left, target, point - 1);
+    }
+    void Travarse(Node root, int point)
+    {
+        if(root == null)return;
+        if(point == 0)
+        {
+            ans += root.data;
+            // System.out.println(root.data);
+        }
+        Travarse(root.left, point - 1);
+        Travarse(root.right, point + 1);
+    }
+    long verticallyDownBST(Node root,int target)
+    {
+        // System.out.println(root.data + " " + root.right.data);
+        ans = 0;
+        t = null;
+        axis = Integer.MIN_VALUE;
+        findTarget(root, target, 0);
+        if(axis == Integer.MIN_VALUE)return -1;
+        Travarse(t, 0);
+        return ans - target;
+        
+    }
+}

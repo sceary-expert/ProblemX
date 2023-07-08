4eaf0fce9635cd8d89ca18c085e84cb1d3c399ff
Dhananjoy Dey <84634405+sceary-expert@users.noreply.github.com>
2023-07-08 11:30:29 +0530

    Added solution - LeetHub

---
+++ Find triplets with zero sum - GFG/find-triplets-with-zero-sum.js
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+//Initial Template for javascript
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for(;i<t;i++)
+    {
+        let n = parseInt(readLine());
+        let arr = new Array(n);
+        let input_ar1 = readLine().split(' ').map(x=>parseInt(x));
+        for(let i=0;i<n;i++)
+            arr[i] = input_ar1[i];
+        let obj = new Solution();
+        if(obj.findTriplets(arr, n)){
+            console.log("1");
+        }
+        else{
+            console.log("0");
+        }
+        
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for javascript
+
+/**
+ * @param {number[]} arr
+ * @param {number} n
+ * @returns {boolean}
+*/
+
+class Solution {
+    //Function to find triplets with zero sum.
+    findTriplets(arr, n)
+    {
+        //your code here
+        for(let i = 0; i < n; i++)
+        {
+            const isPresent = new Map();
+            let target = 0 - arr[i];
+            for(let j = i + 1; j < n; j++)
+            {
+                let curr = arr[j];
+                if(isPresent.get(target - curr) == true)
+                {
+                    return 1;
+                }
+                isPresent.set(curr, true);
+            }
+            
+            // for(let j = i + 1; j < n; j++)
+            // {
+            //     for(let k = j + 1; k < n; k++)
+            //     {
+            //         if(arr[i] + arr[j] + arr[k] == 0)return 1;
+            //     }
+            // }
+        }
+        return 0;
+    }
+}

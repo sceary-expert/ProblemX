9de732ee6415165d90b5597cc9edb8d88d2e9e1e
Dhananjoy Dey <84634405+sceary-expert@users.noreply.github.com>
2023-09-03 13:43:43 +0530

    Added solution - LeetHub

---
+++ Check if Tree is Isomorphic - GFG/check-if-tree-is-isomorphic.cpp
@@ -0,0 +1,141 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = new Node(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+
+// } Driver Code Ends
+/*Complete the function below
+Node is as follows:
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution{
+  public:
+  int Data(Node* n)
+  {
+      if(n == NULL)return -1;
+      return n->data;
+  }
+    // Return True if the given trees are isomotphic. Else return False.
+    bool Travarse(Node* r1, Node* r2)
+    {
+        if(Data(r1) != Data(r2))return false;
+        if(Data(r1) == -1)return true;
+        bool f1 = (Travarse(r1->left, r2->left) &
+        Travarse(r1->right, r2->right));
+        bool f2 = (Travarse(r1->left, r2->right) &
+        Travarse(r1->right, r2->left));
+        return (f1|f2);
+    }
+    bool isIsomorphic(Node *root1,Node *root2)
+    {
+        
+        return Travarse(root1, root2);
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+	scanf("%d ",&t);
+    while(t--)
+    {
+        string s1,s2;
+		getline(cin,s1);
+		getline(cin,s2);
+		Node* root1 = buildTree(s1);
+		Node* root2 = buildTree(s2);
+		Solution obj;
+		if(obj.isIsomorphic(root1,root2))
+		    cout<<"Yes"<<endl;
+		else
+		    cout<<"No"<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
